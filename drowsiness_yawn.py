00b6fe0c3d33f45323734041240a98bd166e7267
Ramadasu91 <121422571+Ramadasu91@users.noreply.github.com>
2024-12-18 10:46:15 +0530

    Update drowsiness_yawn.py

--- drowsiness_yawn.py
+++ drowsiness_yawn.py
@@ -82,7 +82,7 @@ if uploaded_file:
 
     # Load Haar Cascade and dlib predictor
     detector = cv2.CascadeClassifier("haarcascade_frontalface_default%20(1).xml")
-    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks%20(1).dat')
 
     cap = cv2.VideoCapture(video_path)
     stframe = st.empty()
